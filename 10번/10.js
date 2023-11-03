b9d568c2e6083e0fa37e8e01d20c00d200c7fb99
heeneeee <144337252+heeneeee@users.noreply.github.com>
2023-11-03 20:09:26 +0900

    1주차 과제 완료

---
+++ 10번/10.js
@@ -0,0 +1,5 @@
+function func(x) {
+  return x * 2;
+}
+const result = func(2);
+console.log(result);
---
+++ 11번/11.js
@@ -0,0 +1,9 @@
+// const time = new Date();
+// console.log(getHours());
+// // if(time = 10){
+// //     console.log("good morning")
+// // } else if () {
+// //     console.log("good afternoon")
+// // } else() {
+// //     console.log("good evening")
+// // }
